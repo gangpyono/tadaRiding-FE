db00fb562feb32ade17af8a23eda7b0d1272fe39
박하린 <phr0210@naver.com>
2021-10-12 13:33:53 +0900

    feat: input and Button elements

---
+++ front/src/elements/Button.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = (props) => {
+  const {
+    width,
+    margin,
+    padding,
+    borderRadius,
+    size,
+    color,
+    backgroundColor,
+    _onClick,
+    isFloat,
+    children,
+  } = props;
+
+  const styles = {
+    width: width,
+    margin: margin,
+    padding: padding,
+    borderRadius: borderRadius,
+    size: size,
+    color: color,
+    backgroundColor: backgroundColor,
+  };
+
+  if (isFloat) {
+    return (
+      <>
+        <FloatButton {...styles} _onClick={_onClick}>
+          {children}
+        </FloatButton>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <ElButton {...styles} _onClick={_onClick}>
+        {children}
+      </ElButton>
+    </>
+  );
+};
+
+Button.defaultProps = {
+  width: "100px",
+  margin: "",
+  padding: "5px",
+  borderRadius: "50px",
+  size: "",
+  color: "black",
+  backgroundColor: "#90EE90",
+  isFloat: false,
+  children: null,
+  _onClick: () => {},
+};
+
+const ElButton = styled.button`
+  width: ${(props) => props.width};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  box-sizing: border-box;
+  border: none;
+  border-radius: ${(props) => props.borderRadius};
+  font-size: ${(props) => props.size};
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.backgroundColor};
+  cursor: pointer;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.12), 0 2px 5px rgba(0, 0, 0, 0.12);
+`;
+
+const FloatButton = styled.button`
+  position: fixed;
+  right: 50px;
+  bottom: 50px;
+  width: 50px;
+  height: 50px;
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  box-sizing: border-box;
+  border: none;
+  border-radius: ${(props) => props.borderRadius};
+  font-size: ${(props) => props.size};
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.backgroundColor};
+  cursor: pointer;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.12), 0 2px 5px rgba(0, 0, 0, 0.12);
+`;
+
+export default Button;
--- front/src/elements/Input.js
+++ front/src/elements/Input.js
@@ -1,8 +1,99 @@
 import React from "react";
-import { StyledComponent } from "styled-components";
+import styled from "styled-components";
 
 const Input = (props) => {
-  return <></>;
+  const {
+    width,
+    height,
+    margin,
+    padding,
+    label,
+    multiLine,
+    type,
+    placeholder,
+    enterSubmit,
+    _onChange,
+  } = props;
+
+  const styles = {
+    width: width,
+    height: height,
+    margin: margin,
+    padding: padding,
+  };
+
+  if (multiLine) {
+    return (
+      <>
+        <div style={{ margin: "30px" }}>
+          <Textarea
+            {...styles}
+            rows={10}
+            label={label}
+            placeholder={placeholder}
+            enterSubmit={enterSubmit}
+            _onChange={_onChange}
+          />
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div style={{ margin: "30px" }}>
+        <ElInput
+          {...styles}
+          label={label}
+          type={type}
+          placeholder={placeholder}
+          enterSubmit={enterSubmit}
+          _onChange={_onChange}
+        />
+      </div>
+    </>
+  );
 };
 
+Input.defaultProps = {
+  width: "300px",
+  height: "",
+  margin: "",
+  padding: "",
+  label: "",
+  type: "text",
+  multiLine: false,
+  placeholder: "텍스트를 입력하세요",
+  enterSubmit: () => {},
+  _onChange: () => {},
+};
+
+const ElInput = styled.input`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 2px solid gray;
+  :focus {
+    outline: none;
+    border-bottom: 2px solid #90ee90;
+  }
+`;
+
+const Textarea = styled.textarea`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  line-height: 1.5em;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 2px solid gray;
+  :focus {
+    outline: none;
+    border-bottom: 2px solid #90ee90;
+  }
+`;
 export default Input;
