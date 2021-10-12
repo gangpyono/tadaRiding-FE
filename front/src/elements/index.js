6c0e7bbb263c0cc565e7a46d3aedea4b195e3c31
shrkdvy <shrkdvy123@gmail.com>
2021-10-12 13:46:21 +0900

    feat : Grid element 추가

---
+++ front/src/elements/index.js
@@ -0,0 +1,8 @@
+import Button from './Button';
+import Grid from './Grid';
+import Image from './Image';
+import Input from './Input';
+import LikeButton from './LikeButton';
+import Text from './Text';
+
+export { Button, Grid, Image, Input, LikeButton, Text };
