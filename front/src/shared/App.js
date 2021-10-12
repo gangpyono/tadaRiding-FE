14d7e936fe54cc04502fa492d92302a86fb75309
박하린 <phr0210@naver.com>
2021-10-12 22:14:38 +0900

    App.js 주석 삭제

--- front/src/shared/App.js
+++ front/src/shared/App.js
@@ -9,10 +9,10 @@ import Header from "./Header";
 // pages
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
-// import MyPage from '../pages/MyPage';
-// import PostDetail from '../pages/PostDetail';
-// import PostList from '../pages/PostList';
-// import PostWrite from '../pages/PostWrite';
+import MyPage from "../pages/MyPage";
+import PostDetail from "../pages/PostDetail";
+import PostList from "../pages/PostList";
+import PostWrite from "../pages/PostWrite";
 
 function App() {
   return (
@@ -21,10 +21,10 @@ function App() {
       <Header />
       <Route path="/Login" exact component={Login} />
       <Route path="/SignUp" exact component={SignUp} />
-      {/* <Route path="/Mypage" exact component={MyPage} />
+      <Route path="/Mypage" exact component={MyPage} />
       <Route path="/" exact component={PostList} />
       <Route path="/PostWrite" exact component={PostWrite} />
-      <Route path="/PostDetail" exact component={PostDetail} /> */}
+      <Route path="/PostDetail" exact component={PostDetail} />
       {/* </Grid> */}
     </>
   );
