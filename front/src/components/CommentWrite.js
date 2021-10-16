7c2b6b5616291c754c656e03a2c40e2a1aad4caf
spacejay1007 <spacejay1007@gmail.com>
2021-10-16 11:23:29 +0900

    feat:comment(ing)

--- front/src/components/CommentWrite.js
+++ front/src/components/CommentWrite.js
@@ -1,40 +1,13 @@
 import React from "react";
 // elements
 import { Grid, Input, Text, Button } from "../elements/index";
+
 // actions
+import { addCommentDB } from "../redux/modules/comment";
+import moment from "moment";
 
-const CommentWrite = () => {
-  const [comment, setComment] = React.useState("");
-
-  return (
-    <React.Fragment>
-      <Text padding="16px 0px" size="1em" bold color="#727e82">
-        댓글
-      </Text>
-      <Grid isFlex width="100%" margin="0 auto">
-        <Input
-          multiLine
-          height="50px"
-          padding="10px"
-          width="100%"
-          radius="15px"
-          value={comment}
-          placeholder="내용을 입력하세요"
-          _onChange={(e) => setComment(e.target.value)}
-        ></Input>
-
-        <Button
-          isShadow
-          backgroundColor="#ffffee"
-          padding="10px"
-          margin="0px 0px 0px 10px"
-          // _onClick={addComment}
-        >
-          댓글작성
-        </Button>
-      </Grid>
-    </React.Fragment>
-  );
+const CommentWrite = (props) => {
+  return <React.Fragment></React.Fragment>;
 };
 
 export default CommentWrite;
--- front/src/pages/PostDetail.js
+++ front/src/pages/PostDetail.js
@@ -1,39 +1,67 @@
 import React from "react";
 import styled from "styled-components";
-import { Grid, Text, Button, Image } from "../elements/index";
+import { Grid, Text, Button, Image, Input } from "../elements/index";
 
-import CommentWrite from "../components/CommentWrite";
-import CommentList from "../components/CommentList";
+// import CommentWrite from "../components/CommentWrite";
+// import CommentList from "../components/CommentList";
 
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 // axios instance
 import apis from "../lib/apis";
 // actions
-import { actionCreators as postActions } from "../redux/modules/post";
 import { addCommentDB, deleteCommentDB } from "../redux/modules/comment";
+import { actionCreators as postActions } from "../redux/modules/post";
+import moment from "moment";
+// import { addCommentDB, deleteCommentDB } from "../redux/modules/comment";
 
 const PostDetail = (props) => {
+  const user = useSelector((state) => state.user.userInfo);
   const dispatch = useDispatch();
   const _postUid = props.match.params.postUid;
   const [post, setPost] = React.useState("");
+  const [comment, setcomment] = React.useState("");
+  const [cmt, setCmt] = React.useState("");
+  const today = moment().format("YYYY-MM-DD");
+  const commentData = {
+    commentDesc: cmt,
+    commentDate: today,
+  };
 
   React.useEffect(() => {
     apis
       .getDetail(_postUid)
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
         const post = res.data.post;
         setPost(post);
+        const comment = res.data.comments;
+        setcomment(comment);
       })
       .catch((err) => console.log(err));
     console.log("디테일 페이지입니다.");
   }, []);
-
   console.log(post);
+  console.log(comment);
+  const onChangeComment = (e) => {
+    setCmt(e.target.value);
+    console.log("내용입력");
+  };
+  const onClickComment = () => {
+    const _commentData = {
+      user: user.userUid,
+      commentDesc: cmt,
+      commentDate: today,
+    };
+    addCommentDB(commentData, post.postUid);
+    const newComments = [...comment, _commentData];
+    setcomment(newComments);
+    setCmt("");
+  };
+
   const [postLikeCnt, setPostLikeCnt] = React.useState(post.postLikeCnt);
   const [like, setLike] = React.useState(post.postLikeState);
 
@@ -74,7 +102,6 @@ const PostDetail = (props) => {
             <Text size="1em">1</Text>
           </Grid>
         </Grid>
-
         <Grid
           width="100%"
           padding="55px"
@@ -86,7 +113,6 @@ const PostDetail = (props) => {
           <Text size="2em" bold margin="20px 0px 0px 20px" color="#727e82">
             {post.postTitle}
           </Text>
-
           <Grid isFlex>
             {/* 이미지 */}
             <Grid width="300px">
@@ -165,7 +191,6 @@ const PostDetail = (props) => {
               </Grid>
             </Grid>
           </Grid>
-
           {/* 내용 */}
           <Grid width="100%">
             <Text padding="10px 0px" size="1em" bold color="#727e82">
@@ -181,34 +206,66 @@ const PostDetail = (props) => {
               {post.postDesc}
             </Text>
           </Grid>
-          <CommentWrite postUid={props.postUid} />
-        </Grid>
-        <Grid margin="0px 0px 50px 0px">
-          {comments.map((comment, idx) => {
-            return (
-              <div>
-                <Grid isFlex>
-                  <Text size="1em">
-                    {comment.userUid}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    {comment.commentDate}
-                  </Text>
-                  <Button
-                    borderRadius="15px"
-                    backgroundColor="transparent"
-                    width="30px"
-                  >
-                    <DeleteIcon />
-                  </Button>
-                </Grid>
+          {/* <CommentWrite Comment={commentData} /> */}
 
-                <Text size="1em" width="100%" padding="20px 0px 10px 0px">
-                  {comment.commentDesc}
-                </Text>
-                <Line />
-              </div>
-            );
-          })}
+          <Text padding="16px 0px" size="1em" bold color="#727e82">
+            댓글
+          </Text>
+          <Grid isFlex width="100%" margin="0 auto">
+            <Input
+              multiLine
+              height="50px"
+              padding="10px"
+              width="100%"
+              radius="15px"
+              value={cmt}
+              placeholder="내용을 입력하세요"
+              _onChange={onChangeComment}
+            ></Input>
+
+            <Button
+              isShadow
+              backgroundColor="#ffffee"
+              padding="10px"
+              margin="0px 0px 0px 10px"
+              _onClick={onClickComment}
+            >
+              댓글작성
+            </Button>
+          </Grid>
         </Grid>
+        {comment ? (
+          <Grid margin="0px 0px 50px 0px">
+            {comment.map((comments, idx) => {
+              return (
+                <Grid>
+                  <Grid isFlex>
+                    <Text size="1em">
+                      {comments.userUid}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      {comments.commentDate}
+                    </Text>
+                    <Button
+                      borderRadius="15px"
+                      backgroundColor="transparent"
+                      width="30px"
+                      _onClick={() => {
+                        deleteCommentDB(post.postUid, comments.commentUid);
+                        console.log(comments.commentUid);
+                      }}
+                    >
+                      <DeleteIcon />
+                    </Button>
+                  </Grid>
+
+                  <Text size="1em" width="100%" padding="20px 0px 10px 0px">
+                    {comments.commentDesc}
+                  </Text>
+                  <Line />
+                </Grid>
+              );
+            })}
+          </Grid>
+        ) : null}
       </Grid>
     </React.Fragment>
   );
